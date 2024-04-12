6217d90926fa950790fa4a9d1674a6ed7a0842b3
zazasyach <164740232+zazasyach@users.noreply.github.com>
2024-04-12 23:02:55 +0700

    Add files via upload

---
+++ storage/framework/views/7a0ff21cb6896f66c379b2c282caf8dc.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\pemframe_6\resources\views/profile.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/fdba168338317f970fdbe6b02d4270a3.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Laravel\pemframe_y\resources\views/home.blade.php ENDPATH**/ ?>
